345a39b6ba52cd7b2a406e50dde689f9fd9ae576
Glitch (band-sheer-sunset) <none>
2022-09-04 16:21:49 +0000

    🍦🏣 Checkpoint ./commands/log.js:3602252/2 ./commands/küfür-engel.js:3602252/2470

---
+++ commands/küfür-engel.js
@@ -0,0 +1,29 @@
+const { Client, EmbedBuilder, PermissionsBitField } = require("discord.js");
+
+module.exports = {
+  name: "küfür",
+  description: "Sunucudaki küfürleri engeller.",
+  type: 1,
+  options: [{
+    type: 1,
+    name: "engel",
+    description: "Sunucudaki küfürleri engeller.",
+    options: [{
+      type: 3,
+      name: "durum",
+      description: "Sistemin Aktif/Pasif Durumunu Ayarlar",
+      required: true,
+      choices:[{name:"Aktif",value:"aktif"},{name:"Pasif",value:"pasif"}]
+    }]
+  }],
+
+  run: async(client, interaction, db) => {
+
+     if(!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+       return interaction.reply({ embeds: [{ color: 0xED4245, description: ":x: **|** Bu komutu kullanmak için `Mesajları Yönet` yetkisine ihtiyacın var." }], ephemeral: true })
+     }
+    
+   
+  }
+
+};
