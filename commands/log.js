7adbfa39742601716b6bfdef6a8111b5a4f6cd2d
Glitch (band-sheer-sunset) <none>
2022-09-04 11:46:50 +0000

    👚👽 Checkpoint ./commands/log.js:3602252/754

--- commands/log.js
+++ commands/log.js
@@ -16,9 +16,22 @@ module.exports = {
     }]
   }],
 
-  run: async(client, interaction) => {
+  run: async(client, interaction, db) => {
 
-    interaction.reply({ embeds: [ new EmbedBuilder().setDescription(`Pong! ***${client.ws.ping}ms***`) ], ephemeral: true })
+    
+    const log = db.fetch(`log_${interaction.guild.id}`)
+    if(log) return interaction.reply({ embeds: [{
+      description: ":x: **|** Mod-Log sistemi zaten aktif olarak çalışıyor.",
+      color: 0xED4245
+    }]})
+    
+    const channel = interaction.options.getChannel("channel");
+    
+    db.set(`log_${interaction.guild.id}`, { logChannel: channel.id })
+    interaction.reply({ embeds: [{
+      description: "✅ **|** Mod-Log sistemi için kanal başarıyla <#"+channel.id+"> olarak ayarlandı.",
+      color: 0x57F287
+    }] })
 
   }
 
