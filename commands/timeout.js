270976480ff25c5cf8ec6b1f030358dd671c0454
Glitch (band-sheer-sunset) <none>
2022-09-04 13:13:14 +0000

    🏙🌜 Checkpoint ./commands/timeout.js:3602252/1296 ./package.json:3602252/1136

---
+++ commands/timeout.js
@@ -0,0 +1,38 @@
+const { Client, EmbedBuilder } = require("discord.js");
+const ms = require("ms")
+
+module.exports = {
+  name: "timeout",
+  description: "Birini sunucudan banlayabilirsiniz.",
+  options: [{
+    type: 6,
+    name: "user",
+    description: "Bir kullanıcı etiketi girin.",
+    required: true
+  },
+  {
+    type: 3,
+    name: "süre",
+    description: "Bir süre belirtin 1d, 1m gibi",
+    required: true
+  }],
+  type: 1,
+
+  run: async(client, interaction, db) => {
+
+      
+    const user = interaction.options.getMember("user")
+    const time = interaction.option.getString("süre")
+    
+    const timeout = ms(time)
+    if(!timeout) return interaction.reply({ embeds: [{
+      color: 0xED4245,
+      description: ":x: **|** Bu bir süre olarak kabul edilemez."
+    }], ephemeral: true })
+    
+    await user.timeout(timeout)
+    
+
+  }
+
+};
