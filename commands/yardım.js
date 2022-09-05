d8cddbb2a7228e6cb85ca3231d9a060bcfd08389
Glitch (band-sheer-sunset) <none>
2022-09-05 22:01:06 +0000

    💽🌼 Checkpoint ./commands/yardım.js:48006589/3 ./commands/bot-bilgi.js:48006589/4

--- commands/yardım.js
+++ commands/yardım.js
@@ -165,7 +165,7 @@ module.exports = {
 		{ name: 'Sunucu Sayısı', value: `${client.guilds.cache.size}`},
 		{ name: 'Kullanıcı Sayısı', value: `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0) + client.users.cache.size}`, inline: true},
 		{ name: 'Kanal Sayısı', value: `${client.channels.cache.size}`, inline: true},
-		{ name: 'Komut Sayısı', value: `9`},
+		{ name: 'Komut Sayısı', value: `20`},
 		{ name: 'Bot Versiyon', value: `**BETA**`, inline: true},
 		{ name: 'Discord.JS Versiyon', value: `${version}`, inline: true},
 		{ name: 'Node Versiyon', value: `${process.version}`, inline: true},
