51563a8ecdcbf624d55478c51d60527241425410
Glitch (knotty-rustic-steel) <none>
2022-09-09 11:39:29 +0000

    🎫🐣 Checkpoint ./events/interactionCreate.js:3602252/4298

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -31,15 +31,54 @@ module.exports = async(client, interaction) => {
       
       const value = interaction.values[0]
       if(value === "first_option") {
-        
+          const embed = new EmbedBuilder()
+      .setColor("Blue")
+      .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
+      .setDescription(`Merhaba değerli kullanıcımız, şuan size ve sizlere toplam **28** etkileşimli komut ile beraber hizmet etmekteyim, bir sorun oluşursa [destek sunucu]([http://discord.gg/altyapilar)ma sizleri beklerim.`)
+      .addFields(
+        {
+          name: "/ban-list",
+          value: "Banlı kişileri görürsünüz.",
+          inline: true
+        },
+        {
+          name: "/ban-list",
+          value: "Banlı kişileri görürsünüz.",
+          inline: true
+        },
+      )
+      .setFooter({ text: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
+      .setTimestamp()
+          
+      interaction.update({ embeds: [embed] })    
       }
       
       if(value === "second_option") {
+          const embed = new EmbedBuilder()
+      .setColor("Blue")
+      .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
+      .setDescription(`Merhaba değerli kullanıcımız, şuan size ve sizlere toplam **28** etkileşimli komut ile beraber hizmet etmekteyim, bir sorun oluşursa [destek sunucu]([http://discord.gg/altyapilar)ma sizleri beklerim.`)
+      .addFields(
         
+      )
+      .setFooter({ text: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
+      .setTimestamp()
+          
+      interaction.update({ embeds: [embed] })  
       }
       
       if(value === "third_option") {
+          const embed = new EmbedBuilder()
+      .setColor("Blue")
+      .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
+      .setDescription(`Merhaba değerli kullanıcımız, şuan size ve sizlere toplam **28** etkileşimli komut ile beraber hizmet etmekteyim, bir sorun oluşursa [destek sunucu]([http://discord.gg/altyapilar)ma sizleri beklerim.`)
+      .addFields(
         
+      )
+      .setFooter({ text: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
+      .setTimestamp()
+          
+      interaction.update({ embeds: [embed] })  
       }
       
     }
