82111881ac55240d469aae1e72d823b5de73fd72
Glitch (knotty-rustic-steel) <none>
2022-09-09 11:15:25 +0000

    🍓📺 Checkpoint ./commands/yardım.js:3602252/909:48006589/287

--- commands/yardım.js
+++ commands/yardım.js
@@ -2,7 +2,7 @@ const { Client, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Sele
 const Discord = require("discord.js")
 module.exports = {
   name: "yardım",
-  description: "Botun yardım menüsüne bakarsın!",
+  description: "Wegna'Botun yardım menüsüne bakarsın!",
   type: 1,
   options: [],
 
@@ -13,11 +13,35 @@ module.exports = {
     .setColor("Blue")
     .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
     .setDescription(`Merhaba değerli kullanıcımız, şuan size ve sizlere toplam **28** etkileşimli komut ile beraber hizmet etmekteyim, bir sorun oluşursa [destek sunucu]([http://discord.gg/altyapilar)ma sizleri beklerim.`)
-    .addFields({ name: "Butonlar > 🔧", value: "Moderasyon komutlarını sana gösterir.", inline: true })
+    .addFields({ name: "Menü > 🔧", value: "Moderasyon komutlarını sana gösterir.", inline: true }, { name: "Menü >  👤", value: "Kullanıcı komutlarını sana gösterir.", inline: true }, { name: "Menü >  🤖", value: "Bot komutlarını sana gösterir.", inline: true })
     .setFooter({ text: interaction.user.tag, iconURL: interaction.user.displayAvatarURL({ dynmaic: true }) })
     .setTimestamp()
     
-    interaction.reply({ embeds: [embed] })
+    const row = new ActionRowBuilder()
+			.addComponents(
+				new SelectMenuBuilder()
+					.setCustomId('select')
+					.setPlaceholder('Hiçbir şey seçilmedid')
+					.addOptions(
+						{
+							label: '💪 Koruma menüsü',
+							description: 'This is a description',
+							value: 'first_option',
+						},
+						{
+							label: '🤖 Bot menüsü',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+            {
+							label: '🎉 Çekiliş menüsü',
+							description: 'This is also a description',
+							value: 'second_option',
+						},
+					),
+			);
+    
+    interaction.reply({ embeds: [embed], components: [row] })
   }
 
 };
